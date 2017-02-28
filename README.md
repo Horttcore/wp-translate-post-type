0d32b621cabe2cce6a9fc794e0339c3d0f8b8634
Ralf Hortt <me@horttcore.de>
2017-02-28 14:26:37 +0100

    v1.0.0
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# wp-translate-post-type
+# WordPress translation tool for Post Types
+
 A small tool for helping german authors to translate their post types
+
+## Changelog
+
+### v1.0.0
+
+* Initial commit
--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,69 @@
-var $singular = $('#custom-post-type-name-singular'),
+var $singularEN = $('#custom-post-type-name-singular-en'),
+	$pluralEN = $('#custom-post-type-name-plural-en'),
+ 	$singular = $('#custom-post-type-name-singular'),
 	$plural = $('#custom-post-type-name-plural'),
 	$copy = $('.copy-to-clipboard');
 
 $singular.keyup(function(){
-	var val = '' == $singular.val() ? 'Post' : $singular.val();
-	$('.post-type-name-singular.post-type-name-uppercase').text(val);
-	$('.post-type-name-singular.post-type-name-lowercase').text(val.toLowerCase());
+
+	var val = '' == $singular.val() ? 'Post' : $singular.val(),
+		valEN = '' == $singularEN.val() ? 'Post' : $singularEN.val();
+
 	$('input[data-translation*="%%Post%%"]').each(function(i,e){
 		jQuery(this).val(jQuery(this).data('translation').replace('%%Post%%', val));
 	});
+
 	$('input[data-translation*="%%post%%"]').each(function(i,e){
 		jQuery(this).val(jQuery(this).data('translation').replace('%%post%%', val.toLowerCase()));
 	});
+
+});
+
+$singularEN.keyup(function(){
+
+	var val = '' == $singular.val() ? 'Post' : $singular.val(),
+		valEN = '' == $singularEN.val() ? 'Post' : $singularEN.val();
+
+	$('.post-type-name-singular.post-type-name-uppercase').text(valEN);
+	$('.post-type-name-singular.post-type-name-lowercase').text(valEN.toLowerCase());
 });
 
 $plural.keyup(function(){
-	var val = '' == $plural.val() ? 'Post' : $plural.val();
-	$('.post-type-name-plural.post-type-name-uppercase').text(val);
-	$('.post-type-name-plural.post-type-name-lowercase').text(val.toLowerCase());
+
+	var val = '' == $plural.val() ? 'Post' : $plural.val(),
+		valEN = '' == $pluralEN.val() ? 'Post' : $pluralEN.val();
+
 	$('input[data-translation*="%%Posts%%"]').each(function(i,e){
 		jQuery(this).val(jQuery(this).data('translation').replace('%%Posts%%', val));
 	});
+
 	$('input[data-translation*="%%posts%%"]').each(function(i,e){
 		jQuery(this).val(jQuery(this).data('translation').replace('%%posts%%', val.toLowerCase()));
 	});
+
+});
+
+$pluralEN.keyup(function(){
+
+	var val = '' == $plural.val() ? 'Post' : $plural.val(),
+		valEN = '' == $pluralEN.val() ? 'Post' : $pluralEN.val();
+
+	$('.post-type-name-plural.post-type-name-uppercase').text(valEN);
+	$('.post-type-name-plural.post-type-name-lowercase').text(valEN.toLowerCase());
+
 });
 
 $copy.click(function(e){
+
 	e.preventDefault();
+
 	jQuery(this).prev().find('input').select();
+
 	try {
-	    var successful = document.execCommand('copy');
-	    var msg = successful ? 'successful' : 'unsuccessful';
-	  } catch (err) {
-	    console.log('Oops, unable to copy');
-	  }
+		var successful = document.execCommand('copy');
+		var msg = successful ? 'successful' : 'unsuccessful';
+	} catch (err) {
+		console.log('Oops, unable to copy');
+	}
+	
 });
